f01674db0cedfd124a4e8e265b19748a5ea1c6a0
kykt35 <kykt35@gmail.com>
2019-05-01 21:44:33 +0900

    Update pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -21,6 +21,8 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   asyncData() {
     return {
@@ -34,6 +36,49 @@ export default {
     buttonText() {
       return this.isCreateMode ? '新規登録' : 'ログイン'
     }
+  },
+  methods: {
+    async handleClickSubmit() {
+      if (this.isCreateMode) {
+        try {
+          await this.register({ ...this.formData })
+          this.$notify({
+            type: 'success',
+            title: 'アカウント作成完了',
+            message: `${this.formData.id}として登録しました`,
+            position: 'bottom-right',
+            duration: 1000
+          })
+          this.$router.push('/posts/')
+        } catch {
+          this.$notify.error({
+            title: 'アカウント作成完了',
+            message: 'すでに登録されているか。不正なユーザーIDです',
+            position: 'bottom-right',
+            duration: 1000
+          })
+        }
+      } else {
+        try {
+          await this.login({ ...this.formData })
+          this.$notify({
+            type: 'success',
+            title: 'ログイン成功',
+            message: `${title.formData.id}としてログインしました`,
+            position: 'bottom-right',
+            duration: 1000
+          })
+        } catch(e) {
+          this.$notify.error({
+            title: 'ログイン失敗',
+            message: '不正なユーザーIDです',
+            position: 'bottom-right',
+            duration: 1000
+          })
+        }
+      }
+    },
+    ...mapActions(['login', 'register'])
   }
 }
 </script>

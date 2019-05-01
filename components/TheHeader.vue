1db750f1c1520f97d2fb1fdcb6ce4db550cfa705
kykt35 <kykt35@gmail.com>
2019-05-01 20:36:33 +0900

    Create TheHeader.vue

---
+++ components/TheHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-menu mode="horizontal" :router="true">
+    <el-menu-item index="1" style="pointer-events:none;">
+      Nuxt Diary
+    </el-menu-item>
+    <el-menu-item index="2" :route="{ path: '/posts/'}">
+      投稿一覧
+    </el-menu-item>
+  </el-menu>
+</template>

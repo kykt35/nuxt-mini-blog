4cea09e28b1160555b153b8ecee53cb17b98bfae
kykt35 <kykt35@gmail.com>
2019-05-02 13:08:32 +0900

    Update store index.js

--- app/store/index.js
+++ app/store/index.js
@@ -20,5 +20,13 @@ export const actions = {
     const user = await this.$axios.$get(`/users/${id}.json`)
     if (!user.id) throw new Error('Invalid user')
     commit('setUser', { user })
+  },
+  async register({ commit }, { id }) {
+    const payload = {}
+    payload[id] = { id }
+    await this.$axios.$patch(`/users.json`, payload)
+    const user = await this.$axios.$get(`/users/${id}.json`)
+    if (!user.id) throw new Error('Invalid user')
+    commit('setUser', { user })
   }
 }
